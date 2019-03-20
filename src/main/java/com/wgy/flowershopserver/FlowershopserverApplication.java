7c61bb8d35b660bd9f6261fdc4770ea260c1640a
irons <guangyu.wu@dleader.cn>
2019-03-21 02:03:55 +0800

    first commit

---
+++ src/main/java/com/wgy/flowershopserver/FlowershopserverApplication.java
@@ -0,0 +1,13 @@
+package com.wgy.flowershopserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FlowershopserverApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FlowershopserverApplication.class, args);
+    }
+
+}
